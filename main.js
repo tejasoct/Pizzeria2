923f3d64bdf6c800805df007d01ed4105ea3bffc
tejasoct <145278076+tejasoct@users.noreply.github.com>
2024-02-01 17:33:16 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+menu_list_array = [ "Veg Margherita Pizza", "Double Cheese Pizza", "Cheese Blast Pizza", "Chicken Pizza", "Pasta Pizza", "Tandoori Chicken Pizza", "Mushroom Pizza", "Pepperoni Pizza", "Cheese Corn Pizza",                                         ]; //Add more Pizza item names
+
+function getmenu()
+{
+    var htmldata="";
+    for(var i=0; i<menu_list_array.length; i++)
+    {
+        htmldata=htmldata+ menu_list_array[i] + '<br>'
+    }
+    //Give the appropriate id name as display_menu 
+    document.getElementById("display_menu").innerHTML = htmldata;
+}
+
+function add_suggestion(){
+    //Give the appropriate id name as add_item
+	var item = document.getElementById("add_item").value;
+    //Use the push() function to push the item into menu_list_array
+    menu_list_array.push(item);
+}
+
